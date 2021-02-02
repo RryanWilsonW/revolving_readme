515727e4cbee3bb36855dd4ab881bd9837ef20bc
Ryan Wilson <RryanWilsonW@gmail.com>
2021-02-02 14:58:22 -0500

    git ignore implemented, and progress made on js file.

--- README.md
+++ README.md
@@ -1,25 +1,34 @@
-#Title
-Revolving-ReadMEs
+  
+  # Title [![License: {LicenseBadge}]({firstLicenseLink})]({secondLicenseLink})
+  
+  
+  ## Table of Contents
+  -[Description](#description)
+  -[Questions](#questions)
+  -[License](#license)
+  -[Video](#video)
+  
+  ## Description
+  Project Description: 
+  
+  Installation Instructions: 
+  
+  Usage Information: 
+  
+  Contribution Guidelines: 
+  
+  Test Instructions: 
+  
+  ## Questions
+  GitHub Repository: <a href=""></a>
+  Contact Info: 
+  Email:   
+  LinkedIn: 
 
-#Table of Contents
+  ## License
+  This page is covered under the license of IBM.
 
+  ## Video
+  ![Screen Recording of Project]({data.linkToVideo})
 
-#Description, Installation, Usage, Contributing, and Tests
-Project Description: revolving readmes is a program designed to dynamically generate a readme file without haveing to type in all of the extra stuff.
-
-Installation Instructions: Deadlights jack lad schooner scallywag dance the hempen jig carouser broadside cable strike colors. Bring a spring upon her cable holystone blow the man down spanker Shiver me timbers to go on account lookout wherry doubloon chase. Belay yo-ho-ho keelhaul squiffy black spot yardarm spyglass sheet transom heave to.
-
-Usage Information: Deadlights jack lad schooner scallywag dance the hempen jig carouser broadside cable strike colors. Bring a spring upon her cable holystone blow the man down spanker Shiver me timbers to go on account lookout wherry doubloon chase. Belay yo-ho-ho keelhaul squiffy black spot yardarm spyglass sheet transom heave to.
-
-Contribution Guidelines: Deadlights jack lad schooner scallywag dance the hempen jig carouser broadside cable strike colors. Bring a spring upon her cable holystone blow the man down spanker Shiver me timbers to go on account lookout wherry doubloon chase. Belay yo-ho-ho keelhaul squiffy black spot yardarm spyglass sheet transom heave to.
-
-Test Instructions: Deadlights jack lad schooner scallywag dance the hempen jig carouser broadside cable strike colors. Bring a spring upon her cable holystone blow the man down spanker Shiver me timbers to go on account lookout wherry doubloon chase. Belay yo-ho-ho keelhaul squiffy black spot yardarm spyglass sheet transom heave to.
-
-#Questions
-GitHub Repository: <a href="https://github.com/RryanWilsonW/revolving_readme">RryanWilsonW</a>
-Contact Info: I can be contacted over either email or mt LinkedIn account.
-Email: RryanWilsonW@gmail.com  
-LinkedIn: RryanWilsonW
-
-
-
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,93 @@
+const fs = require('fs');
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+  let licenseBadge = '';
+  let firstLicenseLink = '';
+  let secondLicenseLink = '';
+
+ function renderLicenseBadge(license) {
+  if(license === 'MIT') {
+    licenseBadge = 'MIT'
+  }
+  if (license === 'IBM') {
+    licenseBadge = 'IPL 1.0'
+  }
+  if(license === 'GNU') {
+    licenseBadge = 'GPL v3' 
+  }
+  else {
+    licenseBadge = ''
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license === 'MIT') {
+    firstLicenseLink = 'https://img.shields.io/badge/License-MIT-yellow.svg'
+    secondLicenseLink = 'https://opensource.org/licenses/MIT'
+  }
+  if (license === 'IBM') {
+    firstLicenseLink = 'https://img.shields.io/badge/License-IPL%201.0-blue.svg'
+    secondLicenseLink = 'https://opensource.org/licenses/IPL-1.0'
+  }
+  if(license === 'GNU') {
+    firstLicenseLink = 'https://img.shields.io/badge/License-GPLv3-blue.svg'
+    secondLicenseLink = 'https://www.gnu.org/licenses/gpl-3.0'
+  }
+  else {
+    firstLicenseLink = ''
+    secondLicenseLink = ''
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  renderLicenseBadge()
+  renderLicenseLink()
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  fs.writeFile(
+  'README.md',
+  `  
+  # Title [![License: {LicenseBadge}]({firstLicenseLink})]({secondLicenseLink})
+  ${data.projectName}
+  
+  ## Table of Contents
+  -[Description](#description)
+  -[Questions](#questions)
+  -[License](#license)
+  -[Video](#video)
+  
+  ## Description
+  Project Description: ${data.description}
+  
+  Installation Instructions: ${data.installation}
+  
+  Usage Information: ${data.usage}
+  
+  Contribution Guidelines: ${data.contribution}
+  
+  Test Instructions: ${data.test}
+  
+  ## Questions
+  GitHub Repository: <a href="${data.gitLink}">${data.gitUsername}</a>
+  Contact Info: ${data.contactInfo}
+  Email: ${data.email}  
+  LinkedIn: ${data.linkedIn}
 
-`;
+  ## License
+  This page is covered under the license of ${data.license}.
+
+  ## Video
+  ![Screen Recording of Project]({data.linkToVideo})
+
+  `,
+  (err) => {err ? console.log(err) : console.log('NEWREADME Generated!!')});
 }
 
 module.exports = generateMarkdown;
+
